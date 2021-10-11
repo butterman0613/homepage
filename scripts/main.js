7deebf074f5f05e699f2297e4a17b403beceaf27
butterman0613 <77366266+butterman0613@users.noreply.github.com>
2021-10-10 21:13:07 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,12 @@ const btn = document.getElementById("switcher-button");
 function switchProfilePic() {
 	const pic = document.getElementById("profilepic");
 	if(pic.alt === "Profile Photo") {
-		pic.src = "/images/profilepic2.jpg";
+		pic.src = "./images/profilepic2.jpg";
 		pic.alt = "Profile Photo 2";
 	}
 	else {
 		pic.alt = "Profile Photo";
-		pic.src = "/images/profilepic.jpg";
+		pic.src = "./images/profilepic.jpg";
 	}
 }
 
