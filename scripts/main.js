113dadd3449157419da2e17b032a0bc1a90ce3d2
butterman0613 <zyche@ucdavis.edu>
2021-10-10 20:30:45 -0700

    add files
    add files

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById("switcher-button");
+
+function switchProfilePic() {
+	const pic = document.getElementById("profilepic");
+	if(pic.alt === "Profile Photo") {
+		pic.src = "/images/profilepic2.jpg";
+		pic.alt = "Profile Photo 2";
+	}
+	else {
+		pic.alt = "Profile Photo";
+		pic.src = "/images/profilepic.jpg";
+	}
+}
+
+btn.addEventListener("click", switchProfilePic);
